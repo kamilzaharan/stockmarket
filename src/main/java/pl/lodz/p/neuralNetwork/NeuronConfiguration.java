55aea870389275ae4e5ad3088bb8d75a60e76e04
PSkrocki <ppskrocki@gmail.com>
2016-07-01 15:03:27 +0200

    add neuralNetworkPackage with neuronConfiguration

---
+++ src/main/java/pl/lodz/p/neuralNetwork/NeuronConfiguration.java
@@ -0,0 +1,49 @@
+package pl.lodz.p.neuralNetwork;
+
+
+public class NeuronConfiguration {
+	
+	double alpha;
+	double beta;
+	double momentum;
+	boolean isBias;
+
+	public NeuronConfiguration(double alpha, double beta, double momentum, boolean isBias){
+		this.alpha=alpha;
+		this.beta=beta;
+		this.momentum=momentum;
+		this.isBias=isBias;
+	}
+	
+	public double getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(double alpha) {
+		this.alpha = alpha;
+	}
+
+	public double getBeta() {
+		return beta;
+	}
+
+	public void setBeta(double beta) {
+		this.beta = beta;
+	}
+
+	public double getMomentum() {
+		return momentum;
+	}
+
+	public void setMomentum(double momentum) {
+		this.momentum = momentum;
+	}
+
+	public boolean isBias() {
+		return isBias;
+	}
+
+	public void setBias(boolean isBias) {
+		this.isBias = isBias;
+	}
+}
