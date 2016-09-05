6ec47ebc0fad92407857a149f470e05886b62e5f
KHibner <katarzyna.hibner@interia.pl>
2016-09-05 22:41:31 +0200

    restored StatisticEquations

--- src/main/java/pl/lodz/p/equations/StatisticEquations.java
+++ src/main/java/pl/lodz/p/equations/StatisticEquations.java
@@ -1,6 +1,9 @@
 package pl.lodz.p.equations;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import pl.lodz.p.model.Company;
+import pl.lodz.p.model.CompanyStockValue;
 
 import java.util.List;
 
@@ -14,8 +17,8 @@ public class StatisticEquations {
 
         double average=0;
 
-        for (Double aListOfStockValuesLastPrice : listOfStockValuesLastPrice) {
-            average += aListOfStockValuesLastPrice;
+        for(int i=0;i<listOfStockValuesLastPrice.size();i++){
+            average+=listOfStockValuesLastPrice.get(i);
         }
         average/=listOfStockValuesLastPrice.size();
         return average;
@@ -26,8 +29,8 @@ public class StatisticEquations {
 
         double variance=0;
 
-        for (Double aListOfStockValuesLastPrice : listOfStockValuesLastPrice) {
-            variance += Math.pow((aListOfStockValuesLastPrice - average), 2);
+        for(int i=0;i<listOfStockValuesLastPrice.size();i++){
+            variance+=Math.pow((listOfStockValuesLastPrice.get(i)-average),2);
         }
         variance/=listOfStockValuesLastPrice.size();
 
@@ -40,16 +43,16 @@ public class StatisticEquations {
     }
 
     public double getStockValuesMedian(Double[] companySortedArray ) {
-        double median;
+        double median=0;
         int medianId = companySortedArray.length;
-
-        if (medianId % 2 == 0) {
-            medianId /= 2;
-            median = (companySortedArray[medianId - 1] + companySortedArray[medianId]) / 2;
+        if(medianId%2==0){
+            medianId/=2;
+            median= (companySortedArray[medianId-1]+companySortedArray[medianId])/2;
             return median;
-        } else {
-            medianId /= 2;
-            median = companySortedArray[medianId + 1];
+        }
+        else{
+            medianId/=2;
+            median =companySortedArray[medianId+1];
             return median;
         }
     }
